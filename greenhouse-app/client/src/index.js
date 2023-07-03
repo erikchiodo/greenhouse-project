842b93051d9718b12050027f974ba8abb6712b1e
erikchiodo <erikchiodo@gmail.com>
2023-07-02 22:55:10 -0400

    Folder Structure w/ some TODOs

---
+++ greenhouse-app/client/src/index.js
@@ -0,0 +1,16 @@
+// Entry Point for our Application. This file is needed for linking to App (containing all React Components) and to import any custom styling
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.register();
---
+++ greenhouse-app/client/src/utils/queries.js
@@ -0,0 +1,2 @@
+import { gql } from '@apollo/client';
+// Create queries -- Query Products, Query Checkout, Query All Products, Query Categories, Query User
