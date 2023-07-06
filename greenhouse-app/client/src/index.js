15752d7e20eb0cfac5e8a0a2b4858fadc73d64a5
Elinor Chayo <elliechayo@gmail.com>
2023-07-06 01:34:11 -0400

    corrected import App syntax

--- greenhouse-app/client/src/index.js
+++ greenhouse-app/client/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css';
-import App from './App.js';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
