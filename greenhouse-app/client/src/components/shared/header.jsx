b767de326558493358002847c4705d39c911a0e1
Elinor Chayo <elliechayo@gmail.com>
2023-07-05 20:47:53 -0400

    Set up basic structure for React app: HTML index file with meta tags, favicon, viewport configuration, Google Fonts preconnect, and entry point script

---
+++ greenhouse-app/client/src/components/shared/header.jsx
@@ -0,0 +1,12 @@
+import Logo from "../../assets/logo.png";
+
+
+//icons
+import {
+     PiMagnifyingGlass,
+     PiUser,
+     PiHeart,
+     PiShoppingCartSimpleLight,
+     PiListFill,
+   } from "react-icons/pi";
+   import { useEffect, useState } from "react";
