96101cb637b8f9161f78c2718b5b81b45d34ed9a
ShaneNick <shanetomonia@gmail.com>
2023-07-06 21:45:42 -0400

    deleted files and added Section.js

--- greenhouse-app/server/models/FAQ.js
+++ greenhouse-app/server/models/FAQ.js
@@ -1,20 +1,14 @@
-<<<<<<< Updated upstream
 const mongoose = require('mongoose');
 
-=======
-const mongoose = require(‘mongoose’);
->>>>>>> Stashed changes
+
 const faqSchema = new mongoose.Schema({
     id: {
         type: Number,
         required: true,
         unique: true
     },
-<<<<<<< Updated upstream
-    type: {
-=======
+
     common_name: {
->>>>>>> Stashed changes
         type: String,
         required: true,
         trim: true
@@ -26,16 +20,9 @@ const faqSchema = new mongoose.Schema({
     }
 }, {
 });
-<<<<<<< Updated upstream
+
 
 const FAQ = mongoose.model('FAQ', faqSchema);
 
-<<<<<<< HEAD
 module.exports = FAQ;
-=======
-const FAQ = mongoose.model(‘FAQ’, faqSchema);
-module.exports = FAQ;
->>>>>>> Stashed changes
-=======
-module.exports = FAQ;
->>>>>>> b6cc010bf3d780d14677e3c78ae6b74239a17d58
+
